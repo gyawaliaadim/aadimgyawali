49ffa6a367b3f4556dc1969d1bbfc81e595d3a9c
Aadim Gyawali <gyawaliaadim123@gmail.com>
2025-09-14 20:08:43 +0545

    Update Files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,11 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <ol className="font-mono text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
+        <p className="font-mono text-sm/6 text-center sm:text-left">
             On progress
-          </li>
+
        
-     </ol>
+     </p>
      </main>
     </div>
   );
